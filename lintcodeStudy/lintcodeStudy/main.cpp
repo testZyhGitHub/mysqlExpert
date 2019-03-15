80dace9fe165f1d23b92e5207e1111124e2f5adf
testZyhGitHub <zyhnanau86@163.com>
2019-03-15 17:46:17 +0800

    first

---
+++ lintcodeStudy/lintcodeStudy/main.cpp
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include<cstdlib>
+
+void main1()
+{
+	int sum = 0;
+	sum = aplusb(2, 5);
+	printf("sum = %d\n", sum); 
+	
+}
+
+void main2()
+{
+	aADDb1();
+}
+
+void main3()
+{
+	aADDb3();
+}
+
+void main()
+{
+	// main1();
+	// main2();
+	
+	main3();
+
+	system("pause");
+}
---
+++ lintcodeStudy/lintcodeStudy/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int aplusb(int aa, int bb);
+void aADDb1();
+void aADDb2();
+void aADDb3();
+void aADDb4();
+
+#endif
